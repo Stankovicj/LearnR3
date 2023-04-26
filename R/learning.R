a55e671148cae4931e3fea2ec669639220e286ed
Jelena Stankovic <stankovic@clin.au.dk>
2023-04-26 13:21:05 +0200

    7 Data management

--- R/learning.R
+++ R/learning.R
@@ -1,3 +1,104 @@
-library(tidyverse)
-
 #Chapter 7 - Data management
+#Load packages
+
+library(tidyverse)
+library(NHANES)
+
+# Looking at data
+glimpse(NHANES)
+
+#Selecting columns
+select(NHANES, Age)
+
+select(NHANES, Age, Weight, BMI)
+
+select(NHANES, -HeadCirc)
+
+select(NHANES, starts_with("BP"))
+
+select(NHANES, ends_with("Day"))
+
+select(NHANES, contains("Age"))
+
+# Creating an object // Create smaller NHANES dataset
+nhanes_small <- select(NHANES, Age, Gender, BMI,
+                       Diabetes, PhysActive, BPSysAve,
+                       BPDiaAve, Education)
+
+# Renaming columns
+nhanes_small <- rename_with(nhanes_small,
+                            snakecase::to_snake_case)
+
+nhanes_small
+
+# Renaming specific columns
+nhanes_small <- rename(nhanes_small, sex = gender)
+nhanes_small
+
+# Trying out the pipe
+colnames(nhanes_small)
+
+nhanes_small %>%
+    colnames()
+
+nhanes_small %>%
+    select(phys_active) %>%
+    rename(physically_active = phys_active)
+
+# Exercise 7.8
+nhanes_small %>%
+    select(bp_sys_ave, education)
+
+nhanes_small %>%
+    rename(bp_sys = bp_sys_ave,
+           bp_dia = bp_dia_ave)
+
+select(nhanes_small, bmi, contains("age"))
+
+nhanes_small %>%
+    select(bmi,
+    contains("age"))
+
+nhanes_small %>%
+    select(starts_with("bp")) %>%
+    rename(bp_systolic = bp_sys_ave)
+
+# Filtering
+nhanes_small %>%
+    filter(phys_active != "No")
+
+nhanes_small %>%
+    filter(bmi >= 25)
+
+# Combining logical operators
+nhanes_small %>%
+    filter(bmi >= 25 & phys_active == "No")
+
+nhanes_small %>%
+    filter(bmi >= 25 | phys_active == "No")
+
+
+# Arrange data
+nhanes_small %>%
+    arrange(desc(age))
+
+nhanes_small %>%
+    arrange(education, age)
+
+# Transforming data
+nhanes_small %>%
+    mutate(age = age * 12,
+           logged_bmi = log(bmi))
+
+nhanes_small %>%
+    mutate(old = if_else(age >=30, "Yes", "No"))
+
+
+
+
+
+
+
+
+
+
